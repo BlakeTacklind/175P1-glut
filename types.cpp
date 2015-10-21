b9542ce8e882351e5c67ba5f3d1ad352a56e5907
Blake Tacklind <blake@tacklind.com>
2015-10-21 00:35:09 -0700

    forgot to include types.cpp

---
+++ types.cpp
@@ -0,0 +1,78 @@
+#include "types.h"
+#include <math.h>
+
+/* check for vector equality */
+bool operator==(const pnt3& a, const pnt3& b){
+  return a.x == b.x && a.y == b.y && a.z == b.z;
+}
+
+/* dot product of vectors */
+float operator*(const pnt3& a, const pnt3& b){
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+/* vector multiplied by a scaler */
+pnt3 operator*(const pnt3& p, const float i){
+  if (i == 1) return p;
+
+  pnt3 o = {p.x*i, p.y*i, p.z*i};
+  return o;
+}
+
+/* vector multiplied by a scaler */
+pnt3 operator*(const float i, const pnt3& p){
+  if (i == 1) return p;
+
+  pnt3 o = {p.x*i, p.y*i, p.z*i};
+  return o;
+}
+
+/* vector divided by a scaler */
+pnt3 operator/(const pnt3& p, const float i){
+  if (i == 1) return p;
+
+  pnt3 o = {p.x/i, p.y/i, p.z/i};
+  return o;
+}
+
+/* Normalize vector operator */
+pnt3 operator~(const pnt3& p){
+  float d = sqrt(p*p);
+
+  if(d == 1) return p;
+  else       return p/d;
+}
+
+/* negate vector */
+pnt3 operator-(const pnt3& a){
+  if(a==zeroVector) return zeroVector;
+  pnt3 p = {-a.x, -a.y, -a.z};
+  return p;
+}
+
+/* subtracting vectors */
+pnt3 operator-(const pnt3& a, const pnt3& b){
+  if(b==zeroVector) return  a;
+  if(a==zeroVector) return -b;
+  pnt3 p = {a.x-b.x, a.y-b.y, a.z-b.z};
+  return p;
+}
+
+/* Find projection of vector b onto a normal vector a */
+pnt3 operator<<(const pnt3& a, const pnt3& b){
+  float dot = a*b;
+  //if othogonal already return self
+  if(dot==0) return b;
+  //otherwise find the projection
+  return b - (a * dot);
+}
+
+/* cross product of vectors */
+pnt3 operator%(const pnt3& a, const pnt3& b){
+	if(a==unitX && b==unitY) return unitZ;
+	if(a==unitY && b==unitZ) return unitX;
+	if(a==unitZ && b==unitX) return unitY;
+
+	pnt3 p = {a.y*b.z-a.z*b.y,a.z*b.x-a.x*b.z,a.x*b.y-a.y*b.x};
+	return p;
+}
